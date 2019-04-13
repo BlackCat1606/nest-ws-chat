f46d7771f70e7443624c8ef196ebf6630c01e8fe
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2019-04-13 11:29:46 +0200

    added ws gateway to api

---
+++ apps/api/src/app/chat/chat.gateway.spec.ts
@@ -0,0 +1,16 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ChatGateway } from './chat.gateway';
+
+describe('ChatGateway', () => {
+  let gateway: ChatGateway;
+  
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ChatGateway],
+    }).compile();
+    gateway = module.get<ChatGateway>(ChatGateway);
+  });
+  it('should be defined', () => {
+    expect(gateway).toBeDefined();
+  });
+});

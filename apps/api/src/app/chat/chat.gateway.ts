9a4e6d661f2c4b40aaef99d4c27c63ded67016fc
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2019-04-13 20:27:47 +0200

    removed unused imports

--- apps/api/src/app/chat/chat.gateway.ts
+++ apps/api/src/app/chat/chat.gateway.ts
@@ -1,24 +1,18 @@
 import {
   SubscribeMessage,
   WebSocketGateway,
-  OnGatewayConnection,
-  OnGatewayInit
+  OnGatewayConnection
 } from '@nestjs/websockets';
 import { Client } from 'socket.io';
 
 @WebSocketGateway()
-export class ChatGateway implements OnGatewayInit, OnGatewayConnection {
-  @SubscribeMessage('message')
-  handleMessage(client: Client, payload: any): string {
-    return 'Hello world!';
+export class ChatGateway implements OnGatewayConnection {
+  handleConnection(client: Client): void {
+    console.log('handleConnection', client.id);
   }
 
   @SubscribeMessage('joinServer')
   handleJoinServer(client: Client, payload: string): void {
     console.log('joinServer', { payload, client });
-  }
-
-  handleConnection(client: Client) {
-    console.log('handleConnection', client.id);
   }
 }

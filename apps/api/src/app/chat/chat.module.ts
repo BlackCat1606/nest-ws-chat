d451b2dcd477fb4907fc2f7a649130647843c598
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2019-06-02 21:33:03 +0200

    rooms api service

--- apps/api/src/app/chat/chat.module.ts
+++ apps/api/src/app/chat/chat.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+
 import { ChatGateway } from './chat.gateway';
 import { UsersService } from './users/users.service';
+import { RoomsService } from './rooms/rooms.service';
 
 @Module({
-  providers: [ChatGateway, UsersService]
+  providers: [ChatGateway, UsersService, RoomsService]
 })
 export class ChatModule {}
